edaa5d38d4a13d8943ae082e82e7c3b41987019d
ntbchau <ntbchau2301@gmail.com>
2023-05-15 22:36:09 +0200

    State

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        address: 'Cologne',
+        friend: 'Liem'
+    }
     render() {
+        let name = 'Chau'
         return (
-            <div> Hello my component</div>
+            <Fragment>
+                {console.log(name + ` is my name`)}
+                <div> Hello my component</div>
+                <div> My name is {name} and my friend is {this.state.friend} </div>
+                <div> We stay in {this.state.address}</div>
+            </Fragment>
         )
     }
 }
