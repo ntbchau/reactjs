182d3824b3168b784861b68c457761894cb0fb70
ntbchau <ntbchau2301@gmail.com>
2023-05-16 09:08:52 +0200

    setState

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -9,13 +9,23 @@ class MyComponent extends React.Component {
         console.log('Hit the button')
         alert('Please click me')
     }
+    handleOnChange = (event) => {
+        this.setState({
+            friend: event.target.value
+
+        })
+    }
+
     render() {
+        console.log(this.state)
         let name = 'Chau'
         return (
             <Fragment>
-                {console.log(name + ` is my name`)}
+                {/* {console.log(name + ` is my name`)} */}
                 <div> Hello my component</div>
-                <div> My name is {name} and my friend is {this.state.friend} </div>
+                <div> My name is {name} and my friend is {this.state.friend}
+                    <input value={this.state.friend} type="text" onChange={(event) => this.handleOnChange(event)} /> </div>
+
                 <div> We stay in {this.state.address}</div>
                 <button onClick={() => this.handleClickButton()}> Click Me</button>
             </Fragment>
