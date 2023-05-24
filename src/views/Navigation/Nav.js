28ea595d7ccd52adb0cfda69e72ea75bd90e5af9
ntbchau <ntbchau2301@gmail.com>
2023-05-24 14:50:00 +0200

    NavLink vs Link

--- src/views/Navigation/Nav.js
+++ src/views/Navigation/Nav.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import './Nav.scss'
 class Navigation extends React.Component {
     render() {
         return (
             <div className="topnav">
-                <a className="active" href="/">Home</a>
-                <a href="/todos">ToDos</a>
-
-                <a href="/about">About</a>
+                <NavLink to="/" activeClassName="active">
+                    Home
+                </NavLink>
+                <NavLink to="/todos" activeClassName="active">
+                    ToDo
+                </NavLink>
+                <NavLink to="/about" activeClassName="active">
+                    About
+                </NavLink>
+                {/* <Link to="/">Home</Link>
+                <Link to="/todos">ToDos</Link>
+                <Link to="/about">About</Link> */}
             </div>
         )
     }
